8572ee5ae2863d28204a13215515c9ca2ea9c7b3
nidhalpc <92153864+nidhalpc@users.noreply.github.com>
2021-11-08 21:53:52 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Miss Helena*:  ```" + msg + "```"
+    return "✅ *PC-SER*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Miss Helena*:  ```" + msg + "```"
+    return "🛑 *PC-SER*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Miss Helena*:  ```" + msg + "```"
+    return "⏺️ *PC-SER*:  ```" + msg + "```"
 }
 
 
